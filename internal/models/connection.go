38d27a5c4801143ca5d45a8d0bd838d5f57e3fb3
Blank1148 <121607452+Blank1148@users.noreply.github.com>
2025-02-09 20:14:45 +0530

    Update connection.go

--- internal/models/connection.go
+++ internal/models/connection.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func Connect() {
 	// PostgreSQL connection string format: "user=username password=password dbname=mydb sslmode=disable"
-	connection, err := gorm.Open(postgres.Open("host=localhost user=root password=Abishek@15 dbname=go-auth port=5432 sslmode=disable"), &gorm.Config{})
+	connection, err := gorm.Open(postgres.Open("host=localhost user=postgres password=Abishek@15 dbname=postgres port=5432 sslmode=disable"), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connect to the database")
