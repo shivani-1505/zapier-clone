ac9e23f5681de65604eb4b2bbceebd7178a1a2b5
Blank1148 <121607452+Blank1148@users.noreply.github.com>
2025-02-09 18:43:18 +0530

    Update and rename internal/database/database.go to internal/database.go

---
+++ internal/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"go-auth-main/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	// PostgreSQL connection string format: "user=username password=password dbname=mydb sslmode=disable"
+	connection, err := gorm.Open(postgres.Open("host=localhost user=root password=Abishek@15 dbname=go-auth port=5432 sslmode=disable"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
