b1cfaa9f93600322ec620314bc22904fac516ebf
Blank1148 <121607452+Blank1148@users.noreply.github.com>
2025-02-09 19:13:57 +0530

    Update handlers.go

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -1,18 +1,16 @@
-package routes
+package auth
 
 import (
-	"go-auth-main/controllers"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 
-	app.Post("/api/register", controllers.Register)
-	app.Post("/api/login", controllers.Login)
-	app.Get("/api/user", controllers.User)
-	app.Post("/api/logout", controllers.Logout)
-	app.Post("/api/google-login", controllers.GoogleLogin)
-	app.Post("/api/google-register", controllers.GoogleRegister)
+	app.Post("/api/register", auth.Register)
+	app.Post("/api/login", auth.Login)
+	app.Get("/api/user", auth.User)
+	app.Post("/api/logout", auth.Logout)
+	app.Post("/api/google-login", auth.GoogleLogin)
+	app.Post("/api/google-register", auth.GoogleRegister)
 
 }
