46d83db39b31fea4136d74ee450771ac60a347d8
Blank1148 <121607452+Blank1148@users.noreply.github.com>
2025-02-09 18:52:21 +0530

    Update routes.go

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -1,18 +1,1 @@
-package routes
 
-import (
-	"go-auth-main/controllers"
-
-	"github.com/gofiber/fiber/v2"
-)
-
-func Setup(app *fiber.App) {
-
-	app.Post("/api/register", controllers.Register)
-	app.Post("/api/login", controllers.Login)
-	app.Get("/api/user", controllers.User)
-	app.Post("/api/logout", controllers.Logout)
-	app.Post("/api/google-login", controllers.GoogleLogin)
-	app.Post("/api/google-register", controllers.GoogleRegister)
-
-}
