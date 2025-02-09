cc633ade5870c1371a3fcaeccd8023069247c4aa
Blank1148 <121607452+Blank1148@users.noreply.github.com>
2025-02-09 18:57:39 +0530

    Update connection.go

---
+++ internal/models/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"go-auth-main/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:Abishek@15@/go-auth"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
